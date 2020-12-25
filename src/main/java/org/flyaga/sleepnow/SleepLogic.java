e29e182766a9dfc3f69c7354ceec4d62a9b35b82
0x115 <onemandigitalk@gmail.com>
2020-12-26 01:22:03 +0300

    огромный фикс: теперь работает и с бОльшим кол-вом игроков

--- src/main/java/org/flyaga/sleepnow/SleepLogic.java
+++ src/main/java/org/flyaga/sleepnow/SleepLogic.java
@@ -8,21 +8,22 @@ import org.bukkit.scheduler.BukkitScheduler;
 import static org.bukkit.Bukkit.*;
 
 public class SleepLogic {
-    public static int current_sleeping_players_count; // тут будет число игроков, которые на данный момент спят
     public static double players_count_to_skip; // тут будет число игроков, необходимое для скипа ночи
+    public static int current_sleeping = 0; //
     public static List<LivingEntity> current_sleeping_players_entity = new ArrayList<>(); // тут будет список игроков, которые спят в кровати
     public static List<Player> players; // тут будет список игроков в онлайне
     public static World world = getServer().getWorld("world"); // тут получаем мир по его названию
     public static LivingEntity playerEnt; // объявляем энтити, этим энтити будет игрок
-    public static final int ToEightPlayersDivision = 3; // берем всех игроков на сервере -> если их ДО ВОСЬМИ ЧЕЛОВЕК, то делим их на число из этой переменной, и в результате деления получим число игроков, необходимых для скипа
-    public static final long delay = 20L; // задержка плагина (в тиках)
+    public static final int ToEightPlayersDivision = 2; // берем всех игроков на сервере -> если их ДО ВОСЬМИ ЧЕЛОВЕК, то делим их на число из этой переменной, и в результате деления получим число игроков, необходимых для скипа
+    public static long delay = 20L; // задержка плагина (в тиках)
 
     public static void logicStart() {
+        players = world.getPlayers(); // получаем всех игроков в онлайне в список с типом Player (не путать с LivingEntity)
+        players_count_to_skip = Math.floor(players.size() / ToEightPlayersDivision);
         BukkitScheduler scheduler = getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(SleepNow.getInstance(), new Runnable() { // делаем что-то типа while(true) с задержкой
             @Override
             public void run() {
-                players = world.getPlayers(); // получаем всех игроков в онлайне в список с типом Player (не путать с LivingEntity)
                 long time = world.getTime(); // получаем время в мире
                 boolean isDay = time < 12300 || time > 23850; // условия для определения что есть день, а что есть ночь
                 if (isDay == false) { // если в мире ночь
@@ -34,51 +35,43 @@ public class SleepLogic {
 
 
     public static void playerManipulation(){
-        if (players.size() >= 4 && players.size() <= 8) { // если игроков больше или равно 4, и меньше или равно 8
-            if (current_sleeping_players_entity != null) { // если список спящих (на данный момент) игроков НЕ пуст
-                for (LivingEntity sleeping : current_sleeping_players_entity) { // циклом преобразуем список энтити в один энтити
-                    boolean isSleeping = sleeping.isSleeping();
-                    if (isSleeping == false) { // если данный энтити (а конкретно игрок) не спит
-                        current_sleeping_players_count = -1; // убираем его из счетчика спящих
-                        current_sleeping_players_entity.remove(sleeping); // убираем его из списка спящих энтити
-                    }
-                }
-            }
+        if (players.size() >= 3 && players.size() <= 8) { // если игроков больше или равно 4, и меньше или равно 8
             for (Player players_for : players) { // циклом преобразуем список игроков в одного игрока
                 playerEnt = players_for; // тут один игрок
                 boolean isSleeping = playerEnt.isSleeping();
                 if (isSleeping == true) { // если он спит
-                    if (!current_sleeping_players_entity.isEmpty()) { // и список из спящих игроков НЕ пуст
-                        for (LivingEntity sleeping : current_sleeping_players_entity) { // циклом преобразуем список спящих энтити в один энтити
-                            if (playerEnt.getName().equals(sleeping.getName())) { // если ник игрока равен нику спящего энтити
-                                return; // тогда уходим
-                            }
-                        }
+                    if(current_sleeping_players_entity.contains(playerEnt)){
+                        continue;
                     }
-                    updateAndSet(playerEnt, ToEightPlayersDivision);
+                    else{
+                        current_sleeping_players_entity.add(playerEnt);
+                        current_sleeping = current_sleeping + 1;
+                        displayMessage(playerEnt, 1);
+                        updateAndSet(playerEnt);
+                    }
+                }
+                else if(isSleeping == false && current_sleeping_players_entity.contains(playerEnt)){
+                    current_sleeping_players_entity.remove(playerEnt);
+                    current_sleeping = current_sleeping - 1;
                 }
             }
         }
-        else{ // а иначе, если на сервере мало игроков, то и вовсе этого одного игрока хватит для скипа ночи
-            for(Player players_for : players) {
+        else { // а иначе, если на сервере мало игроков, то и вовсе этого одного игрока хватит для скипа ночи
+            for (Player players_for : players) {
                 updateSingleSleeping(players_for);
             }
         }
     }
 
-    public static void updateAndSet(LivingEntity playerEnt, int PlayersDivision){ // PlayersDivision - определяется в зависимости от того, сколько игроков на сервере
-        current_sleeping_players_entity.add(playerEnt);
-        current_sleeping_players_count = + 1;
-        players_count_to_skip = Math.floor(players.size() / PlayersDivision); // делим количество игроков на PlayersDivision и округляем результат в МЕНЬШУЮ сторону
-        if(current_sleeping_players_count >= (int)players_count_to_skip){ // если количество спящих игроков больше или равно количеству игроков, необходимых для скипа
+    public static void updateAndSet(LivingEntity playerEnt){ // PlayersDivision - определяется в зависимости от того, сколько игроков на сервере
+        if(current_sleeping >= (int)players_count_to_skip){ // если количество спящих игроков больше или равно количеству игроков, необходимых для скипа
             world.setTime(0);
-            current_sleeping_players_count = 0;
+            current_sleeping = 0;
             players_count_to_skip = 0;
+            current_sleeping_players_entity.clear();
+            players = world.getPlayers(); // получаем всех игроков в онлайне в список с типом Player (не путать с LivingEntity)
+            players_count_to_skip = Math.floor(players.size() / ToEightPlayersDivision);
             displayMessage(playerEnt, 2);
-        }
-        else{ // а если игрок в кровати, но все равно игроков не хватает для скипа, то...
-            players_count_to_skip = players_count_to_skip - 1; // отнимаем 1 от количества игроков необходимых для скипа (так как один из игроков уже в кровати)
-            displayMessage(playerEnt, 1);
         }
     }
 
@@ -87,14 +80,17 @@ public class SleepLogic {
         if(isSleeping == true)
 
         {
-            displayMessage(playerEnt, 2);
             world.setTime(0);
+            displayMessage(playerEnt, 2);
         }
     }
 
     public static void displayMessage(LivingEntity playerEnt, int what_display){
         if(what_display == 1){
-            broadcastMessage("Игрок" + " " + playerEnt.getName() + " " + "лег в кровать. Для скипа ночи нужно еще" + " " + (int)players_count_to_skip + " " + "игроков.");
+            int to_skip = (int)players_count_to_skip - current_sleeping;
+            if(to_skip > 0){
+                broadcastMessage("Игрок" + " " + playerEnt.getName() + " " + "лег в кровать. Для скипа ночи нужно еще" + " " + to_skip + " " + "игроков.");
+            }
         }
         if(what_display == 2){
             broadcastMessage("Игрок" + " " + playerEnt.getName() + " " + "спит, скипаем ночь...");
